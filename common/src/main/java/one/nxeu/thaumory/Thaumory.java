ca5344eaefe2c64e96e7620f329283445e4aa3db
Naruse Nia <notshinsin0000@gmail.com>
2026-09-23 13:12:43 +0900

    init: setup project

---
+++ common/src/main/java/one/nxeu/thaumory/Thaumory.java
@@ -0,0 +1,9 @@
+package one.nxeu.thaumory;
+
+public final class Thaumory {
+    public static final String MOD_ID = "thaumory";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/one/nxeu/thaumory/fabric/client/ThaumoryFabricClient.java
@@ -0,0 +1,10 @@
+package one.nxeu.thaumory.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ThaumoryFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
